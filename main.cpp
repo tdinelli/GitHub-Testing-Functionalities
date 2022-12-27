139e7a270e668722994831e992f86eedb40b0ea1
timoteo dinelli <timoteo.dinelli@polimi.it>
2022-12-27 11:18:50 +0100

    added wrong file

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	std::cout << "Eccoci qua" << std::endl;
 	ciao
+	eccoci questa riga non va
 	std::cout << "Now=?" << std::endl;
 	std::cout << "Hello world!" << std::endl;
 	return 0;
