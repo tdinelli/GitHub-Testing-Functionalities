c4abca2abb2580609d836fd6e85b0a57ba70ae85
timoteo dinelli <timoteo.dinelli@polimi.it>
2022-12-27 11:18:50 +0100

    Revert "added wrong file"
    This reverts commit 4bc6c3e65c38d31584df4d6fb3b7f3352f3d4b76.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@ int main()
 {
 	std::cout << "Eccoci qua" << std::endl;
 	ciao
-	eccoci questa riga non va
 	std::cout << "Now=?" << std::endl;
 	std::cout << "Hello world!" << std::endl;
 	return 0;
