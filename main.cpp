697f95d55b387707d48ec4ded9bfc8063e3f6540
timoteo dinelli <timoteo.dinelli@polimi.it>
2022-12-27 11:18:06 +0100

    added right line

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 
 int main()
 {
+	std::cout << "Eccoci qua" << std::endl;
 	ciao
- 	std::cout << "Now=?" << std::endl;
+	std::cout << "Now=?" << std::endl;
 	std::cout << "Hello world!" << std::endl;
 	return 0;
 }
